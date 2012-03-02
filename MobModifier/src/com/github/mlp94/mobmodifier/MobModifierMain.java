8ede63d058488f7a004e29cfd3d3eeef4a2bc24a
MLP <MLP@Snake.gateway.2wire.net>
2012-03-02 16:34:25 -0600

    Setup workspace

---
+++ MobModifier/src/com/github/mlp94/mobmodifier/MobModifierMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.mlp94.mobmodifier;
+
+/**
+ *
+ * @author MLP
+ */
+public class MobModifierMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
